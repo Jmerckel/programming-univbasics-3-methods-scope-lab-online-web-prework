2315fd64f39a21c3c3dd81850e489a545b8feccd
Jazmin M <jazminmerckel@gmail.com>
2019-07-31 04:19:54 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,9 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  phrase = "It's Dangerous To Go Along! Take this."
+  puts phrase
+end
+
